300ee9048352d6c113178ca43250c76f17548752
Pedro Luiz Cabral Salomon Prado <pedroprado010@gmail.com>
2019-02-04 18:52:12 -0200

    dividir modulo books

--- hibrido/case_01/books-interals/register.js
+++ hibrido/case_01/books-interals/register.js
@@ -1,10 +1,7 @@
 const BookModel = require('./models/Book');
 const ExemplarModel = require('./models/Exemplar');
-const routes = require('./routes');
 
-register('books-module',function*() {
+register('books-internals',function*() {
   yield register.model(BookModel.name, BookModel.schema);
   yield register.model(ExemplarModel.name, ExemplarModel.schema);
-  yield register.route('get', '/books', routes.listBooks);
-  yield register.route('get', '/books/:book_id/exemplar', routes.listExemplars);
 });
