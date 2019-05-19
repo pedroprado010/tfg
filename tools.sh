724a3145c73b3e1ebd6072e6e73cc3be213b3ae2
Pedro Luiz Cabral Salomon Prado <pedroprado010@gmail.com>
2019-05-19 17:57:34 -0300

    add docker-compose

--- tools.sh
+++ tools.sh
@@ -1,7 +1,7 @@
 # DOCKER
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-
+sudo apt get install docker-compose -y
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
